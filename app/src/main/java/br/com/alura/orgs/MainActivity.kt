b559296f4da2a0d62d87c46dc321b480da7bc61b
marcsilva <marciliodevjava@gmail.com>
2024-09-05 19:47:43 -0300

    Atualizadno do codigo

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -1,7 +1,16 @@
 package br.com.alura.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
+class MainActivity : Activity() {
 
-class MainActivity : Activity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Bem vindo(a) ao Orgs! Marcílio", Toast.LENGTH_SHORT).show()
+        val view = View(this)
+        setContentView(view)
+    }
 }
