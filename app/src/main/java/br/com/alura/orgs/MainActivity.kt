9564be1ed9552cfd1b0245af19593cb55e5598f8
marcsilva <marciliodevjava@gmail.com>
2024-09-05 19:47:52 -0300

    Atualizadno do codigo

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -3,6 +3,7 @@ package br.com.alura.orgs
 import android.app.Activity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity() {
@@ -10,7 +11,11 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Toast.makeText(this, "Bem vindo(a) ao Orgs! Marcílio", Toast.LENGTH_SHORT).show()
+        val view_text = TextView(this)
         val view = View(this)
+        view_text.setText("Marcílio Zanatta Bezerra da Silva")
+
         setContentView(view)
+        setContentView(view_text)
     }
 }
