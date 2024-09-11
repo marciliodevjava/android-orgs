6db97603927ea9874418015cbd8929728d61a433
marcsilva <marciliodevjava@gmail.com>
2024-09-11 19:48:38 -0300

    Ajuste na MainActivity

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -3,6 +3,7 @@ package br.com.alura.orgs
 import android.app.Activity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity() {
@@ -10,9 +11,14 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Toast.makeText(this, "Bem vindo(a) ao Orgs! Marcílio", Toast.LENGTH_SHORT).show()
-        val view = View(this)
-
-        setContentView(view)
         setContentView(R.layout.activity_main)
+
+        val nome: TextView = findViewById<TextView>(R.id.nome)
+        val descricao = findViewById<TextView>(R.id.descricao)
+        val valor = findViewById<TextView>(R.id.valor)
+
+        nome.text = "Cesta de Frutas"
+        descricao.text = "Laranja, manga e maça"
+        valor.text = "19.90"
     }
 }
