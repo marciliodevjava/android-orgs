5e59ccc7fcabcf0ae5c4c7ca74488d403a2e747d
marcsilva <marciliodevjava@gmail.com>
2024-09-11 21:41:34 -0300

    Ajuste no xml activity_main

---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -0,0 +1,30 @@
+package br.com.alura.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.util.Arrays
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Bem vindo(a) ao Orgs! Marcílio", Toast.LENGTH_SHORT).show()
+        setContentView(R.layout.activity_main)
+//
+//        val nome: TextView = findViewById<TextView>(R.id.nome)
+//        val descricao = findViewById<TextView>(R.id.descricao)
+//        val valor = findViewById<TextView>(R.id.valor)
+//
+//        nome.text = "Cesta de Frutas"
+//        descricao.text = "Laranja, manga e maça"
+//        valor.text = "19.90"
+
+        val recyclerView= findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListaProdutosAdapter()
+    }
+}
---
+++ app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -0,0 +1,2 @@
+package br.com.alura.orgs.ui.recyclerview.adapter 
+
